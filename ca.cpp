7d352bfc1cd1020eaba59333ddaf4bfc6a596d48
Imran Hussain <47060906+imrannonari@users.noreply.github.com>
2021-09-13 11:41:21 +0500

    Calculator using Switch Case
    C++ Calculator Using the Switch Case statement

---
+++ ca.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << num1+num2;
+            break;
+
+        case '-':
+            cout << num1-num2;
+            break;
+
+        case '*':
+            cout << num1*num2;
+            break;
+
+        case '/':
+            cout << num1/num2;
+            break;
+
+        default:
+            cout << "Error";
+            break;
+    }
+
+    getch();
+}
