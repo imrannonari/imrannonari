c0c62e65d32abbc182590fb767b9319b9da01ebd
Imran Hussain <47060906+imrannonari@users.noreply.github.com>
2021-09-13 11:43:00 +0500

    Right side Align
    C++ right side align Dimond with stars

---
+++ adspace.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int i,j,k;
+	for(i=1;i<=5;i++){
+		for(j=i;j<=5;j++){
+			cout<<" ";
+		}
+		for(k=1;k<=i;k++){
+			cout<<"*";
+		
+	
+		}
+			cout<<endl;
+		
+	}
+}
