7d637055266eff7523dd58c235ea705c8189181a
Imran Hussain <47060906+imrannonari@users.noreply.github.com>
2021-09-13 11:23:46 +0500

    Right diamond stars
    C++ right diamond stars programs

---
+++ star.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int i,j,k;
+	for(i=1;i<=5;i++){
+		for(j=1;j<=i;j++){
+		//	for(k=j;k<=5;k++){
+		//		cout<<" ";
+		//	}
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	getch();
+}
