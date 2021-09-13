490e1508c731b55845ea9cbaa828cb3b8c8c3004
Imran Hussain <47060906+imrannonari@users.noreply.github.com>
2021-09-13 11:21:21 +0500

    Half Dimond

---
+++ halfdimand.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int i,j,k,l;
+	for(i=1;i<=5;i++){
+		for(j=1;j<=i;j++){
+			cout<<" *";
+		}
+		cout<<endl;
+		
+	}
+	for(k=1;k<=5;k++){
+		for(l=k;l<=5;l++){
+			cout<<" *";
+		}
+		cout<<endl;
+			
+		}
+	getch();
+}
